1c100cdbd14756e785b0d62e6e62a57afe0f0506
Slin Castro <consultor1@payphone.app>
2024-08-19 16:00:46 -0500

    include correct and incorrect counter

--- quiz.py
+++ quiz.py
@@ -4,7 +4,7 @@ import json
 with open('questions.json', 'r') as file:
     questions = json.load(file)
 
-def ask_question(q):
+def ask_question(q) -> bool:
     
     print(q["Question"]+"\n")
     print(f"a) {q['a']}")
@@ -15,13 +15,17 @@ def ask_question(q):
     return answer == q["correct"]
 
 
-def quiz():
+def quiz() -> None:
+    nested_correct_answers = 0
     correct_answers = 0
+    wrong_answers = 0
+
 
     while questions:
         question = random.choice(questions)
-        print(f"\n Respuestas correctas seguidas : {correct_answers} \n")
-
+        print(f"\n Respuestas correctas  : {correct_answers}")
+        print(f"\n Respuestas incorrectas : {wrong_answers}")
+        print(f"\n Respuestas correctas seguidas : {nested_correct_answers} \n")
         correct = ask_question(question)
         
         if correct:
@@ -29,15 +33,18 @@ def quiz():
             print("Correct!\n")
             print("-"*20)
             correct_answers = correct_answers + 1
+            nested_correct_answers = nested_correct_answers + 1
+            questions.remove(question)
         else:
             print("X" * 90 + "\n")
             print(f"Incorrect! The correct answer was {question['correct']} :.\n")
             print(question["Question"]+"\n")
             print(f"{question[question['correct']]}\n")
             print("X" * 90)
-            correct_answers =0
+            nested_correct_answers = 0
+            wrong_answers = wrong_answers + 1
         
-        questions.remove(question)
+        
 
     print("You've completed the quiz!")
 
